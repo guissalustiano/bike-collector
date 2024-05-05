9b4678e4a10392d72d9c97532a8af372be02a666
Guilherme Salustiano <guissalustiano@gmail.com>
2024-05-06 01:57:50 +0200

    collect imu

--- collect.py
+++ collect.py
@@ -1,9 +1,13 @@
+import time
 from datetime import datetime
+import csv
 from multiprocessing import Process
 
-def record_raw(timestamp: str):
+from loguru import logger
+
+def record_camera(timestamp: str):
     from picamera2 import MappedArray, Picamera2
-    from picamera2.encoders import H264Encoder
+    from picamera2.encoders import JpegEncoder
     import cv2
 
     def apply_timestamp(
@@ -18,16 +22,24 @@ def record_raw(timestamp: str):
         with MappedArray(request, "main") as m:
             cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
 
+    # Setup camera
     picam2 = Picamera2()
+    picam2.pre_callback = apply_timestamp # add timestamp on top left
     video_config = picam2.create_video_configuration({"size": (1280, 720)})
-    picam2.pre_callback = apply_timestamp
     picam2.configure(video_config)
 
-    encoder = H264Encoder(10000000)
+    encoder = JpegEncoder(q=70)
 
-    picam2.start_recording(encoder, f'{timestamp}-video.h264', pts=f'{timestamp}-video-timestamp.txt')
+    logger.debug(f"Starting recorgind {timestamp}...")
+    picam2.start_recording(encoder, f'{timestamp}-video.mjpeg', pts=f'{timestamp}-video.timestamp.txt')
+    try:
+        while True:
+            time.sleep(10)
+    finally:
+        logger.debug("Stopping recording...")
+        picam2.stop_recording()
 
-def record_sensor(timestamp: str):
+def record_imu_sensor(timestamp: str):
     from mpu6050 import mpu6050
 
     MPU_ADDR = 0x68
@@ -35,15 +47,23 @@ def record_sensor(timestamp: str):
 
     with open(f'{timestamp}-mpu.csv', 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        spamwriter.writerow(["temp", "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z"])
+        csvwriter.writerow(["timestamp", "temp", "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z"])
 
         while True:
+            timestamp = datetime.now().isoformat()
             [accel, gyro, temp] = sensor.get_all_data()
-            spamwriter.writerow([temp, accel['x'], accel['y'], accel['z'], gyro['x'], gyro['y'], gyro['z']])
+            csvwriter.writerow([timestamp, temp, accel['x'], accel['y'], accel['z'], gyro['x'], gyro['y'], gyro['z']])
     
 if __name__ == '__main__':
     timestamp = datetime.now().isoformat()
-    p = Process(target=record_raw, args=(timestamp,))
-    p.start()
+    logger.add(f'{timestamp}.log')
+    p_camera = Process(target=record_camera, args=(timestamp,), daemon=True)
+    p_imu = Process(target=record_imu_sensor, args=(timestamp,), daemon=True)
 
-    p.join()
+    #p_camera.start()
+    p_imu.start()
+
+    time.sleep(10)
+
+    p_imu.terminate()
+    # p_camera.terminate()
